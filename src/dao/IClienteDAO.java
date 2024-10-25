251373c434b4e869f8a4ec7ba654da3e5f47c791
SpitSmoke <jmfelicio.sp@gmail.com>
2024-10-24 23:07:45 -0300

    finalizando o CRUD

---
+++ src/dao/IClienteDAO.java
@@ -0,0 +1,27 @@
+package br.com.Jose.dao;
+
+import br.com.Jose.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+    public boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+
+
+
+
+
+
+
+
+
+
+}
